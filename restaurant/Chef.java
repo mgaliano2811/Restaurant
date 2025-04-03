10956a02a5156d8713ab0f20e4eaf7d15d787182
Lufferly <pug.pugface@gmail.com>
2025-04-03 10:12:03 -0700

    maven project

--- restaurant/Chef.java
+++
@@ -1,8 +0,0 @@
-public class Chef extends Staff{
-    private Table table;
-    public Chef(String lastName, String fistName, int employeeID, String password){
-        super(lastName, fistName, employeeID, password);
-    }
-    @Override
-    public String toString() { return super.toString() + "(Chef)";}
-}
--- restaurant/Manager.java
+++
@@ -1,7 +0,0 @@
-public class Manager extends Staff{
-    public Manager(String lastName, String firstName, int employeeID, String password){
-        super(lastName, firstName, employeeID);
-    }
-    @Override
-    public String toString() { return super.toString() + "(Manager)";}
-}   
