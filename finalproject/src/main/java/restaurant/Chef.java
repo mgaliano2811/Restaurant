30b6556e1b937b0867c90b42ed553aa915c17d6d
Lufferly <pug.pugface@gmail.com>
2025-04-04 14:24:10 -0700

    I hope I dont fuck us for time immemorial:

--- finalproject/src/main/java/restaurant/Chef.java
+++ finalproject/src/main/java/restaurant/Chef.java
@@ -1,7 +1,9 @@
+package restaurant;
+
 public class Chef extends Staff{
     private Table table;
-    public Chef(String lastName, String fistName, int employeeID, String password){
-        super(lastName, fistName, employeeID, password);
+    public Chef(String lastName, String fistName, int employeeID){
+        super(lastName, fistName, employeeID);
     }
     @Override
     public String toString() { return super.toString() + "(Chef)";}
