33354b868b3dd6feb7c1df866b03bbba347cded2
TheFrnchGuy <parismgarcia@gmail.com>
2025-04-17 10:24:44 -0700

    Customers make orders based on menu

--- finalproject/src/main/java/restaurant/Customer.java
+++ finalproject/src/main/java/restaurant/Customer.java
@@ -1,13 +1,38 @@
 package restaurant;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import restaurant.menu.*;
+
 // A basic customer class, they have something they want to eat
 //  This should remain IMMUTABLE, please...
 public class Customer {
-    String name;
-    String foodWant;
+    UUID id;
+    ArrayList<MenuItem> foodWant;
 
     public Customer() {
-        name = "Severian";
-        foodWant = "Lemons";
+        id = UUID.randomUUID();
+        foodWant = makeFoodWant();
+    }
+
+    private ArrayList<MenuItem> makeFoodWant() {
+        ArrayList<MenuItem> foodWant = new ArrayList<>();
+        
+        Menu menu = new Menu();
+
+        foodWant.add(menu.getMenuByType(ItemType.ENTREE).values().iterator().next());
+        foodWant.add(menu.getMenuByType(ItemType.DRINK).values().iterator().next());
+        foodWant.add(menu.getMenuByType(ItemType.DESSERT).values().iterator().next());
+
+        return foodWant;
+    }
+
+    public String getId() {
+        return id.toString();
+    }
+
+    public ArrayList<MenuItem> getFoodWant() {
+        return new ArrayList<>(foodWant);
     }
 }
